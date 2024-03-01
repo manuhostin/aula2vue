18ace62dc6262905e32f84b5e2c95611f8479ba5
manuhostin <hostimmanu@gmail.com>
2024-03-01 15:10:58 -0300

    adiçao dos exemplos da aula 2 de vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,61 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+const nome = 'manu'
+const idade = 16
 
+ function inverter(texto) {
+  return texto.split('').reverse().join('')
+ }
+ function saudacao() {
+  return `Olá, ${nome}`
+
+ }
+ import { ref } from 'vue'
+
+const contador = ref(0)
+
+function decrementar () {
+  contador.value--
+}
+</script>]
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="info">
+      <p>Nome: {{ nome }} </p>
+      <p>Idade: {{ idade }}  anos</p>
+      <hr />
+      <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
+      <p>O nome tem {{ nome.length }}  caracteres</p>
+      <p>O nome invertido é: {{ inverter(nome.toUpperCase()) }} </p>
+      <hr />
+      <p> Exemplo de saudação usando função</p>
+      <p> {{ saudacao() }}  </p>
+    <button @click="contador++">+</button>
+    <button @click="decrementar">-</button>
+    <p>valor do contador: {{ contador }}</p>
+    <div v-if = "contador > 10" class="verde" > <p>Contador maior que 10</p></div>
+    <div v-else class="vermelho" > <p>Contador menor que 10</p></div>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+@import url('https://fonts.googleapis.com/css2?family=Madimi+One&display=swap');
+
+.info {
+font-family: "Madimi One", sans-serif;
+background-color: rgb(0, 0, 0);
+padding: 20px 30px;
+color: rgb(183, 210, 219);
+border-radius: 10px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.vermelho {
+  color: red;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.verde {
+  color: green;
+}
+h1 {
+color: white;
+font-weight: bold;
+font-size: 1.5em;
 }
 </style>
